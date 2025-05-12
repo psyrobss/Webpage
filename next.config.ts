e4e8a91cc75fad4f84a4d993fc32b35d2b57493e
psyrobss <rjspes@yahoo.com.br>
2025-05-12 12:21:22 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 
-// Verifica se o ambiente é de produção (usado pelo GitHub Pages build)
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {

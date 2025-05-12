e88fe6d6eae387cceb39bdc2b80442be03af93e6
psyrobss <rjspes@yahoo.com.br>
2025-05-12 12:18:03 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+// Verifica se o ambiente é de produção (usado pelo GitHub Pages build)
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? '/Webpage' : '',
+
+  assetPrefix: isProd ? '/Webpage/' : '',
+
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
 };
 
 export default nextConfig;

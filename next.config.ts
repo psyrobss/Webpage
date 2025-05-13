01b5751c2faafc82a2059721e7cdf811b4ce53a2
psyrobss <rjspes@yahoo.com.br>
2025-05-13 10:44:48 -0300

    deploy test

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Essencial para GitHub Pages
+  
+    // Define o assetPrefix para o nome do seu repositório
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Webpage' : '',
+
   // Define o basePath para o nome do seu repositório
   basePath: process.env.NODE_ENV === 'production' ? '/Webpage' : '',
-  // Define o assetPrefix para o nome do seu repositório
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Webpage' : '',
+  
   // Desabilita a otimização de imagem padrão do Next.js para exportação estática
   images: {
     unoptimized: true,
   },
-  reactStrictMode: true, // Ou false se estiver causando muitos re-renders em dev com R3F
+
 };
 
 export default nextConfig;
